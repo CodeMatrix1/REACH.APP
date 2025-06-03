a042190dfcc896bde82810a19801b100dbf3b1a3
Ritankar Mondal <ritankarmondal123@gmail.com>
2025-06-03 17:21:44 +0530

    check

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,4 +1,3 @@
-"use client";
 import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 import CategoryLocations from "./CategoryLocations";
 
@@ -16,7 +15,7 @@ const center = {
 
 function Map({ results }) {
   return (
-    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API}>
+    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API}> {/* Use NEXT_PUBLIC_ variable */}
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
@@ -38,4 +37,5 @@ function Map({ results }) {
     </LoadScript>
   );
 }
+
 export default Map;
